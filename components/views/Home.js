434b3e3ad21f8a7974255c398863045b5b6c7946
Sridevi15k <sridevi15k@gmail.com>
2020-11-02 18:18:41 -0600

    Car Warranty Check

--- components/views/Home.js
+++ components/views/Home.js
@@ -6,13 +6,26 @@ export default () => `
         <p>A simple and easy to use reminder application to keep track of your warranties for your purchases, time-sensitive receipts and documents so you get the value for the money you put in as we happily take the effort to send you timely reminders.</p>
   </div>
   <div class="side-bar">
-    <h3>Features</h3>
-    <ul>
-    <li>Simple, affordable and easy to use.</li><br>
-    <li>Hassle free timely reminders.</li><br>
-    <li>Safe and secure accounts/transactions.</li><br>
-    <li>Easy uploads using scan/photograph.</li>
-    </ul>
+  <h1 id="search">Search your Car</h1>
+  <form id="Carform" action="" method="POST">
+      <label for="Carsearch"></label>
+      <select id="Carsearch" name="Carsearch" value="Carsearch">
+          <option id="SelectMake" value="SelectMake">Select Make</option>
+          <option id="chevrolet" value="chevrolet">Chevrolet</option>
+          <option id="honda" value="Honda">Honda</option>
+          <option id="ford" value="Ford">Ford</option>
+          <option id="toyota" value="Toyota">Toyota</option>
+          <option id="gmc" value="GMC">GMC</option>
+    <option id="chrysler" value="chrysler">Chrysler</option>
+    <option id="volkswagen" value="Volkswagen">Volkswagen</option>
+    <option id="jeep" value="Jeep">Jeep</option>
+      </select> <br>
+      <select id="model" name="model">
+            <option id="selectmodel" value="Select Model">Select Model</option>
+        </select>
+        <br>
+        <input type="submit" value="Select" id="select">
+
   </div>
   <div class="side-bar">
   <img src="https://github.com/Sridevi15k/myCapStoneProjectRepo/blob/master/Docs/WebsiteImages/Bunny%20clock.jpg?raw=true" alt="home image">
