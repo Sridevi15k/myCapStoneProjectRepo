b5ae4138abd77999a0f98056590c823e9ff9531a
HPBlack <HPBlack@home.com>
2020-11-08 00:55:28 -0600

    CSS changes

--- components/views/Addproduct.js
+++ components/views/Addproduct.js
@@ -6,36 +6,35 @@ export default () => `
     <hr>
 
   <div>
-    <label for="manufacturer">Manufacturer:</label>
-    <input type="text" id="manufacturer" name="manufacturer_name">
-    <abbr title="required" aria-label="required">*</abbr>
+    <label for="manufacturer">* Manufacturer</label>
+    <input type="text" id="manufacturer" name="manufacturer_name" required>
+
   </div><br><br>
   <div>
-    <label for="name">Product Name:</label>
-    <input type="text" id="productname" name="product_name">
-    <abbr title="required" aria-label="required">*</abbr>
+    <label for="name">* Product Name</label>
+    <input type="text" id="productname" name="product_name" required>
+
   </div><br><br>
   <div>
-    <label for="model">Model:</label>
-    <input type="varchar" id="model" name="model_no">
-    <abbr title="required" aria-label="required">*</abbr>
+    <label for="model">* Model</label>
+    <input type="varchar" id="model" name="model_no" required>
+
   </div><br><br>
   <div>
-    <label for="purchasedate">Date of Purchase:</label>
+    <label for="purchasedate">* Date of Purchase</label>
     <input type="date" placeholder="mm-dd-yyyy" value=""
-    min="01-01-2010" max="12-31-2050" id="purchasedate" name="purchase_date">
-    <abbr title="required" aria-label="required">*</abbr>
+    min="01-01-2010" max="12-31-2050" id="purchasedate" name="purchase_date" required>
+
   </div><br><br>
   <div>
-    <label for="expirydate">Expiry Date:</label>
+    <label for="expirydate">* Expiry Date</label>
     <input type="date" placeholder="mm-dd-yyyy" value=""
-    min="11-01-2020" max="12-31-2050" id="expirydate" name="expiry_date">
-    <abbr title="required" aria-label="required">*</abbr>
+    min="11-01-2020" max="12-31-2050" id="expirydate" name="expiry_date" required>
+
   </div><br><br>
-  <div><br><br>
-    <label for="photo">Attach photo:</label>
+  <div>
+    <label for="photo">Attach photo</label>
     <input type"file" id="photo" name="attach_photo" accept="image/png, image/jpeg"></input>
-    <abbr title="required" aria-label="required">*</abbr>
    </div><br><br>
 
   <div class="button">
--- components/views/Register.js
+++ components/views/Register.js
@@ -5,25 +5,26 @@ export default () => `
 
     <p>Please fill in this form to create an account.</p>
     <hr>
-
-    <label for="First Name"><b>First Name</b></label>
-    <input type="text" placeholder="First Name" name="FirstName" required>
-
-    <label for="Last Name"><b>Last Name</b></label>
-    <input type="text" placeholder="Last Name" name="LastName" required>
-
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <label for="psw-repeat"><b>Retype Password</b></label>
+  <div>
+    <label for="First Name"><b>* First Name</b></label>
+    <input type="text" placeholder="First Name" name="FirstName" required><br><br>
+  </div>
+  <div>
+    <label for="Last Name"><b>* Last Name</b></label>
+    <input type="text" placeholder="Last Name" name="LastName" required><br><br>
+  </div>
+  <div>
+    <label for="email"><b>* Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required><br><br>
+  </div>
+  <div>
+    <label for="psw"><b>* Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required><br><br>
+  </div>
+  <div>
+    <label for="psw-repeat"><b>* Confirm Password</b></label>
     <input type="password" placeholder="Retype Password" name="psw-repeat" required><br><br>
-
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
+  </div>
 
     <p>By creating an account you agree to our <a class="terms" href="#">Terms & Privacy</a></p>
 
