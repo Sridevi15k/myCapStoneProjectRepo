fe5320d747d920005d536be17a31c9605e8118de
Sridevi15k <sridevi15k@gmail.com>
2020-11-08 18:24:21 -0600

    Added Home link to User Nav and other changes

--- components/UserNav.js
+++ components/UserNav.js
@@ -6,7 +6,7 @@ export default toplinks => `
     <ul class="hidden--mobile nav-links">
     ${Links.map(
       link =>
-        `<li><a href="${link.title}" data-navigo><i class="fa fa-fw ${link.icon}"></i> ${link.text}</a></li>`
+        `<li><a id="Nav_${link.title}" href="${link.title}" data-navigo><i class="fa fa-fw ${link.icon}"></i> ${link.text}</a></li>`
     ).join("")}
   </ul>
   </nav>
--- components/views/Search.js
+++ components/views/Search.js
@@ -2,7 +2,7 @@ export default () => `
 <section id="searchbar">
 <div class="content">
 
-    <input type="text" class="search__input" aria-label="search" placeholder="enter your search">
+    <input type="text" class="search_input" aria-label="search" placeholder="enter your search">
     <button class="search__submit" aria-label="submit search"><i class="fas fa-search"></i></button>
 
 </div>
