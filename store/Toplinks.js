a587022a845377ad4a902942eeb171af81b64994
Sridevi15k <sridevi15k@gmail.com>
2020-11-08 18:24:21 -0600

    Added Home link to User Nav and other changes

--- store/Toplinks.js
+++ store/Toplinks.js
@@ -1,4 +1,5 @@
 export default [
+  { title: "Home", text: "Home", icon: "fa-home" },
   { title: "Signout", text: "Signout", icon: "fa-sign-out-alt" },
   { title: "Addproduct", text: "Addproduct", icon: "" },
   { title: "Search", text: "Search", icon: "fa-search" }
